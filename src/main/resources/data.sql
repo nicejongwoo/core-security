8a44e0a60fda8f03a22d54d3bd336bf963de3ff4
jongwoo <leejongwoo.dev@gmail.com>
2021-10-07 22:18:52 +0900

    init account init, logout

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into account (id, age, email, password, role, username)
+values (1, 22, 'user@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'ROLE_USER', 'user');
+insert into account (id, age, email, password, role, username)
+values (2, 32, 'manager@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'ROLE_MANAGER', 'manager');
+insert into account (id, age, email, password, role, username)
+values (3, 33, 'admin@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'ROLE_ADMIN', 'admin');
