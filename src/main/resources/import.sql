288ec3debd3ec1afa24306811ee7665bd84b6124
jongwoo <leejongwoo.dev@gmail.com>
2021-10-12 21:24:11 +0900

    import.sql 한줄로 변경

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,7 @@
 -- password: 123
-insert into account (id, age, email, password, username)
-values (90001, 22, 'user@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'user');
-insert into account (id, age, email, password, username)
-values (90002, 32, 'manager@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'manager');
-insert into account (id, age, email, password, username)
-values (90003, 33, 'admin@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'admin');
+insert into account (id, age, email, password, username) values (90001, 22, 'user@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'user');
+insert into account (id, age, email, password, username) values (90002, 32, 'manager@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'manager');
+insert into account (id, age, email, password, username) values (90003, 33, 'admin@email.com', '{bcrypt}$2a$10$rBb1HWeLjn0pNnVQsv8FjuRLWoLqgcIEVsioo0c0Q5wK21AUDIrl6', 'admin');
 
 insert into role (role_id, role_desc, role_name) values (90001, '사용자권한', 'ROLE_USER');
 insert into role (role_id, role_desc, role_name) values (90002, '매니저권한', 'ROLE_MANAGER');
